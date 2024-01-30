7e9ac695ecebb8afdcfb41969ceba965c6283c53
At216 <142370102+At216@users.noreply.github.com>
2024-01-29 20:55:49 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", e=> {
+    e.waitUntil(
+        caches.open("staticnoisy").then(cache => {
+            return cache.addAll([".", "/app/favicon_notes.png"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
+//Check "."?
